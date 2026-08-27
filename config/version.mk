db627c92a1e8aec18964737317b3574a902551f0
inferno0230 <mail@inferno0230.in>
2026-08-27 14:57:42 +0530

    version: Append CLO tag to ro.custom.system.version
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- config/version.mk
+++ config/version.mk
@@ -4,11 +4,13 @@ CUSTOM_PLATFORM_VERSION := 17.0
 
 CUSTOM_VERSION := $(CUSTOM_BUILD)-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)
 CUSTOM_VERSION_PROP := seventeen
+CLO_SYSTEM_VERSION := $(shell grep -o 'refs/tags/LA\.QSSI[^"]*' manifest/default.xml | sed 's|refs/tags/||')
 
 # PixelOS Platform Version
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.custom.build.date=$(CUSTOM_BUILD_DATE) \
     ro.custom.device=$(CUSTOM_BUILD) \
+    ro.custom.system.version=$(CLO_SYSTEM_VERSION) \
     ro.custom.version=PixelOS_$(CUSTOM_VERSION) \
     net.pixelos.version=$(CUSTOM_VERSION_PROP)
 
